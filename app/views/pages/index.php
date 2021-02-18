8138a9cd8aa14d71d2da7dd360be3404fa6d93c2
LAZYASSKID <lzysskd@gmail.com>
2021-02-18 10:49:27 +0800

    :bug: Styling Errors

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -71,20 +71,6 @@
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col-xl-8 mb-30">
-					<div class="card-box height-100-p pd-20">
-						<h2 class="h4 mb-20">Activity</h2>
-						<div id="chart5"></div>
-					</div>
-				</div>
-				<div class="col-xl-4 mb-30">
-					<div class="card-box height-100-p pd-20">
-						<h2 class="h4 mb-20">Lead Target</h2>
-						<div id="chart6"></div>
-					</div>
-				</div>
-			</div>
 			<div class="card-box mb-30">
 				<h2 class="h4 pd-20">Best Selling Products</h2>
 				<table class="data-table table nowrap">
