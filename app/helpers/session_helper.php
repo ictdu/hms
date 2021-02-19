45e64d5d9d89a6eff0ed544d8398d2dc3f4f96c0
LAZYASSKID <lzysskd@gmail.com>
2021-02-19 23:36:44 +0800

    :bug: Fix flash message not showing up

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -5,10 +5,10 @@
     // Create flash message - flash('register', 'You are now registered');
     // Display in view - echo flash('register_success');
 
-    function flash($name = '', $message = '', $class = 'alert alert-success') 
+    function flash($name = '', $message = '', $class = 'alert alert-success alert-dismissible fade show') 
     {
         if(!empty($name)) {
-            if(!empty($message) && empty($_SESSION['$name'])) {
+            if(!empty($message) && empty($_SESSION[$name])) {
                 if(!empty($_SESSION[$name])) {
                     unset($_SESSION[$name]);
                 }
@@ -19,9 +19,9 @@
 
                 $_SESSION[$name] = $message;
                 $_SESSION[$name . '_class'] = $class;
-            } elseif(empty($message) && !empty($_SESSION['$name'])) {
+            } elseif(empty($message) && !empty($_SESSION[$name])) {
                 $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
-                echo '<div class="'. $class .'" id="msg-flash">' . $_SESSION[$name] . '</div>';
+                echo '<div class="'. $class .'" id="msg-flash">' . $_SESSION[$name] . '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
 
                 unset($_SESSION[$name]);
                 unset($_SESSION[$name . '_class']);
