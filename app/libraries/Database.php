23333019209c59f679412e23fa28457e4b12771d
LAZYASSKID <lzysskd@gmail.com>
2021-02-19 15:52:54 +0800

    :bug: Fix '$dsn' Value

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -19,7 +19,7 @@
         public function __construct()
         {
             // Set DSN
-            $dsn = 'mysql:host=' . $this->host . ';dbname' . $this->dbname;
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
             $options = array(
                 PDO::ATTR_PERSISTENT => true,
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
