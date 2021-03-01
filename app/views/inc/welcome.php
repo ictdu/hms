fb8cc3c5b83c95c065f1390df22a3c955ed7979d
LAZYASSKID <lzysskd@gmail.com>
2021-03-02 01:24:03 +0800

    :100: Display current time

--- app/views/inc/welcome.php
+++ app/views/inc/welcome.php
@@ -4,10 +4,15 @@
             <img src="<?php echo URLROOT ?>/vendors/images/banner-img.png" alt="">
         </div>
         <div class="col-md-8">
-            <h4 class="font-20 weight-500 mb-10 text-capitalize">
-                Welcome to your dashboard <div class="weight-600 font-30 text-blue"><?php if(isset($_SESSION['user_name'])) echo $_SESSION['user_name']; ?></div>
+            <h4 class="font-20 weight-500 mb-10">
+                Welcome to your dashboard <span class="weight-600 font-15 text-blue"><?php if(isset($_SESSION['user_name'])) echo $_SESSION['user_name']; ?></span>
             </h4>
-            <p class="font-18 max-width-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde hic non repellendus debitis iure, doloremque assumenda.</p>
+            <p class="font-18 max-width-600">Current Time: 
+            <?php 
+                date_default_timezone_set ('Asia/Hong_Kong');
+                echo date('l, F d Y (h:i a)'); 
+            ?>
+            </p>
         </div>
     </div>
 </div>
