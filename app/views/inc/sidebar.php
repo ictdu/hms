6e7a7f9450ba924060f1003ecc77df6f2eca1a6d
LAZYASSKID <lzysskd@gmail.com>
2021-03-02 00:23:29 +0800

    :100: Add new logo

--- app/views/inc/sidebar.php
+++ app/views/inc/sidebar.php
@@ -1,7 +1,6 @@
 	<div class="left-side-bar">
 		<div class="brand-logo mb-30 mt-30">
 			<a href="#">
-				<img src="<?php echo URLROOT; ?>/vendors/images/deskapp-logo.svg" alt="" class="dark-logo">
 				<img src="<?php echo URLROOT; ?>/vendors/images/deskapp-logo-white.svg" alt="" class="light-logo">
 			</a>
 			<div class="close-sidebar" data-toggle="left-sidebar-close">
