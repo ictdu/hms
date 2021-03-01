887a8c3808e3f22c8d8e6d974e08bcc0f62567d3
LAZYASSKID <lzysskd@gmail.com>
2021-03-02 06:06:24 +0800

    :100: Blank Config w/ Instructions

---
+++ app/config/config_backup.php
@@ -0,0 +1,25 @@
+<?php
+    // DB Params
+    define('DB_HOST', '');      // Database Host
+    define('DB_USER', '');      // Database User
+    define('DB_PASS', '');      // Database Password
+    define('DB_NAME', '');      // Database Name
+    
+    // Dir for uploads folder
+    // Default: define('UPLOADS', '/hms/public/images/uploads/');
+    define('UPLOADS', '/directory/to/your/uploads/folder/');
+
+    // Site Name
+    define('SITENAME', '');     // Enter Site Name
+
+    // App Root (do not change)
+    define('APPROOT', dirname(dirname(__FILE__))); 
+
+    // When setting up in new env change the 'public' folder dir in '../public/.htaccess'
+    // Example: RewriteBase /hms/public
+    
+    // URL Root
+    // Default: define('URLROOT', 'http://localhost');
+    define('URLROOT', '');
+    
+    
