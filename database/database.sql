8125cb937a0f9f1adedd52cd6cc13e9984ab4996
LAZYASSKID <lzysskd@gmail.com>
2021-03-02 09:55:45 +0800

    :coffee: Updated database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,59 +1,40 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 01, 2021 at 01:42 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hms_db`
---
-
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server version:               5.7.24 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
 -- --------------------------------------------------------
 
---
--- Table structure for table `categories`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `categories` (
-  `id` bigint(6) UNSIGNED NOT NULL,
+-- Dumping structure for table ictdu_hms.categories
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` bigint(6) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `rate` float NOT NULL,
   `description` varchar(255) NOT NULL,
   `capacity` varchar(6) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=1005 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `categories`
---
-
+-- Dumping data for table ictdu_hms.categories: ~1 rows (approximately)
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
 INSERT INTO `categories` (`id`, `name`, `rate`, `description`, `capacity`, `image`, `created_at`, `updated_at`) VALUES
-(1000, 'Regular Room I', 999.99, 'A regular room.', '1-2', 'regular.jpg', '2021-02-20 13:53:14', '2021-02-21 12:04:18');
+	(1000, 'Regular Room I', 999.99, 'A regular room.', '1-2', 'regular.jpg', '2021-02-20 21:53:14', '2021-03-02 08:52:57');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `checkouts`
---
-
-CREATE TABLE `checkouts` (
-  `id` bigint(6) UNSIGNED NOT NULL,
-  `room_number` bigint(6) UNSIGNED NOT NULL,
+-- Dumping structure for table ictdu_hms.checkouts
+CREATE TABLE IF NOT EXISTS `checkouts` (
+  `id` bigint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `room_number` bigint(6) unsigned NOT NULL,
   `full_name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `phone_number` varchar(255) NOT NULL,
@@ -61,44 +42,39 @@ CREATE TABLE `checkouts` (
   `check_in_date` date NOT NULL,
   `check_out_date` date NOT NULL,
   `notes` varchar(255) NOT NULL,
-  `checked_in_by` bigint(6) UNSIGNED NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `checked_in_by` bigint(6) unsigned NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `FK_CHECKED_IN_BY_USERS_ID` (`checked_in_by`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- Dumping data for table ictdu_hms.checkouts: ~1 rows (approximately)
+/*!40000 ALTER TABLE `checkouts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `checkouts` ENABLE KEYS */;
 
---
--- Table structure for table `discounts`
---
-
-CREATE TABLE `discounts` (
-  `id` bigint(6) UNSIGNED NOT NULL,
-  `type` varchar(15) NOT NULL,
+-- Dumping structure for table ictdu_hms.discounts
+CREATE TABLE IF NOT EXISTS `discounts` (
+  `id` bigint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(20) NOT NULL,
   `code` varchar(6) NOT NULL,
   `discount` float NOT NULL,
-  `use_limit` int(6) DEFAULT NULL,
+  `number_of_usage` int(6) DEFAULT NULL,
   `status` varchar(15) NOT NULL DEFAULT 'active',
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `discounts`
---
+-- Dumping data for table ictdu_hms.discounts: ~0 rows (approximately)
+/*!40000 ALTER TABLE `discounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `discounts` ENABLE KEYS */;
 
-INSERT INTO `discounts` (`id`, `type`, `code`, `discount`, `use_limit`, `status`, `created_at`, `updated_at`) VALUES
-(1000, 'promo code', 'LAZY', 50, 10, 'active', '2021-03-01 04:08:51', '2021-03-01 04:09:57');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `guests`
---
-
-CREATE TABLE `guests` (
-  `id` bigint(6) UNSIGNED NOT NULL,
-  `room_number` bigint(6) UNSIGNED NOT NULL,
+-- Dumping structure for table ictdu_hms.guests
+CREATE TABLE IF NOT EXISTS `guests` (
+  `id` bigint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `room_number` bigint(6) unsigned NOT NULL,
   `full_name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `phone_number` varchar(255) NOT NULL,
@@ -106,220 +82,91 @@ CREATE TABLE `guests` (
   `check_in_date` date NOT NULL,
   `check_out_date` date NOT NULL,
   `notes` varchar(255) NOT NULL,
-  `checked_in_by` bigint(6) UNSIGNED NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `checked_in_by` bigint(6) unsigned NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `FK_CHECKED_IN_BY_USERS_ID` (`checked_in_by`),
+  KEY `FK_GUESTS_ROOM_NUMBER_ROOMS_NUMBER` (`room_number`),
+  CONSTRAINT `FK_CHECKED_IN_BY_USERS_ID` FOREIGN KEY (`checked_in_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- Dumping data for table ictdu_hms.guests: ~0 rows (approximately)
+/*!40000 ALTER TABLE `guests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `guests` ENABLE KEYS */;
 
---
--- Table structure for table `invoices`
---
-
-CREATE TABLE `invoices` (
-  `id` bigint(6) UNSIGNED NOT NULL,
-  `number` bigint(6) UNSIGNED NOT NULL,
-  `guest_id` bigint(6) UNSIGNED NOT NULL,
-  `balance` float UNSIGNED NOT NULL,
+-- Dumping structure for table ictdu_hms.invoices
+CREATE TABLE IF NOT EXISTS `invoices` (
+  `id` bigint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `number` bigint(6) unsigned NOT NULL,
+  `guest_id` bigint(6) unsigned NOT NULL,
+  `balance` float unsigned NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'unpaid',
-  `discounted` tinyint(1) NOT NULL DEFAULT 0,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `discounted` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5003 DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- Dumping data for table ictdu_hms.invoices: ~1 rows (approximately)
+/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
 
---
--- Table structure for table `reservations`
---
-
-CREATE TABLE `reservations` (
-  `id` bigint(6) UNSIGNED NOT NULL,
+-- Dumping structure for table ictdu_hms.reservations
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `id` bigint(6) unsigned NOT NULL AUTO_INCREMENT,
   `status` varchar(255) NOT NULL DEFAULT 'pending',
-  `guest` bigint(6) UNSIGNED NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `guest` bigint(6) unsigned NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `FK_RESERVATION_ID_GUEST_ID` (`guest`),
+  CONSTRAINT `FK_RESERVATION_ID_GUEST_ID` FOREIGN KEY (`guest`) REFERENCES `guests` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- Dumping data for table ictdu_hms.reservations: ~0 rows (approximately)
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
 
---
--- Table structure for table `rooms`
---
-
-CREATE TABLE `rooms` (
-  `id` bigint(6) UNSIGNED NOT NULL,
-  `number` bigint(6) UNSIGNED NOT NULL,
+-- Dumping structure for table ictdu_hms.rooms
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id` bigint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `number` bigint(6) unsigned NOT NULL,
   `category` varchar(255) NOT NULL,
   `status` varchar(12) NOT NULL DEFAULT 'available',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQUE` (`number`)
+) ENGINE=InnoDB AUTO_INCREMENT=7002 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `rooms`
---
-
+-- Dumping data for table ictdu_hms.rooms: ~1 rows (approximately)
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
 INSERT INTO `rooms` (`id`, `number`, `category`, `status`, `created_at`, `updated_at`) VALUES
-(1000, 69, 'Regular Room I', 'available', '2021-02-20 17:59:00', '2021-02-28 22:06:28'),
-(1016, 101, 'Regular Room I', 'booked', '2021-02-28 17:43:31', '2021-02-28 22:09:16'),
-(1017, 102, 'Deluxe Room', 'available', '2021-03-01 01:37:01', '2021-03-01 01:46:22');
+	(7000, 69, 'Regular Room I', 'available', '2021-02-21 01:59:00', '2021-03-02 09:05:07');
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` bigint(6) UNSIGNED NOT NULL,
+-- Dumping structure for table ictdu_hms.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(6) unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `gender` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL DEFAULT 'employee',
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8002 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
-
+-- Dumping data for table ictdu_hms.users: ~2 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `password`, `gender`, `role`, `updated_at`, `created_at`) VALUES
-(1, 'Juan', 'Dela Cruz', 'admin', '$2y$10$tdB657rnVTn7Vv3.ODgGUecibuHkDkAk29SU.VBvVZYN4.AQb/XcS', 'male', 'admin', '2021-02-19 10:39:20', '2021-02-19 07:03:44'),
-(1000, 'Jay', 'Beza', 'lazyasskid', '$2y$10$tdB657rnVTn7Vv3.ODgGUecibuHkDkAk29SU.VBvVZYN4.AQb/XcS', 'female', 'employee', '2021-02-22 02:13:09', '2021-02-19 15:13:46');
+	(1, 'Juan', 'Dela Cruz', 'admin', '$2y$10$tdB657rnVTn7Vv3.ODgGUecibuHkDkAk29SU.VBvVZYN4.AQb/XcS', 'male', 'admin', '2021-02-19 18:39:20', '2021-02-19 15:03:44'),
+	(8000, 'Patrick', 'Star', 'employee', '$2y$10$tdB657rnVTn7Vv3.ODgGUecibuHkDkAk29SU.VBvVZYN4.AQb/XcS', 'female', 'employee', '2021-03-02 06:39:11', '2021-02-19 23:13:46');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQUE` (`name`);
-
---
--- Indexes for table `checkouts`
---
-ALTER TABLE `checkouts`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_CHECKED_IN_BY_USERS_ID` (`checked_in_by`);
-
---
--- Indexes for table `discounts`
---
-ALTER TABLE `discounts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `guests`
---
-ALTER TABLE `guests`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_CHECKED_IN_BY_USERS_ID` (`checked_in_by`);
-
---
--- Indexes for table `invoices`
---
-ALTER TABLE `invoices`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_GUEST_INVOICE` (`guest_id`) USING BTREE;
-
---
--- Indexes for table `reservations`
---
-ALTER TABLE `reservations`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FK_RESERVATION_ID_GUEST_ID` (`guest`);
-
---
--- Indexes for table `rooms`
---
-ALTER TABLE `rooms`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQUE` (`number`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` bigint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1007;
-
---
--- AUTO_INCREMENT for table `checkouts`
---
-ALTER TABLE `checkouts`
-  MODIFY `id` bigint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
-
---
--- AUTO_INCREMENT for table `discounts`
---
-ALTER TABLE `discounts`
-  MODIFY `id` bigint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1001;
-
---
--- AUTO_INCREMENT for table `guests`
---
-ALTER TABLE `guests`
-  MODIFY `id` bigint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
-
---
--- AUTO_INCREMENT for table `invoices`
---
-ALTER TABLE `invoices`
-  MODIFY `id` bigint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
-
---
--- AUTO_INCREMENT for table `reservations`
---
-ALTER TABLE `reservations`
-  MODIFY `id` bigint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT for table `rooms`
---
-ALTER TABLE `rooms`
-  MODIFY `id` bigint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1018;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1014;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `guests`
---
-ALTER TABLE `guests`
-  ADD CONSTRAINT `FK_CHECKED_IN_BY_USERS_ID` FOREIGN KEY (`checked_in_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_GUESTS_ROOM_NUMBER_ROOMS_NUMBER` FOREIGN KEY (`room_number`) REFERENCES `rooms` (`number`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `reservations`
---
-ALTER TABLE `reservations`
-  ADD CONSTRAINT `FK_RESERVATION_ID_GUEST_ID` FOREIGN KEY (`guest`) REFERENCES `guests` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
