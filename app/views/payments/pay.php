45cd31b67bf86ac51fd33fac64eb2586813dc3c1
LAZYASSKID <lzysskd@gmail.com>
2021-03-10 01:08:24 +0800

    :100: Added Banks List

--- app/views/payments/pay.php
+++ app/views/payments/pay.php
@@ -104,19 +104,15 @@
                                     <h6>Select your Bank</h6>
                                 </label> <select class="form-control" id="ccmonth">
                                     <option value="" selected disabled>--Please select your Bank--</option>
-                                    <option>Bank 1</option>
-                                    <option>Bank 2</option>
-                                    <option>Bank 3</option>
-                                    <option>Bank 4</option>
-                                    <option>Bank 5</option>
-                                    <option>Bank 6</option>
-                                    <option>Bank 7</option>
-                                    <option>Bank 8</option>
-                                    <option>Bank 9</option>
-                                    <option>Bank 10</option>
+                                    <option>BDO Unibank Inc.</option>
+                                    <option>Bank of the Philippine Islands</option>
+                                    <option>Philippine National Bank</option>
+                                    <option>Land Bank of the Philippines</option>
+                                    <option>China Bank</option>
+                                    <option>Security Bank</option>
                                 </select> </div>
                             <div class="form-group">
-                                <p> <button type="button" class="btn btn-primary "><i class="fas fa-mobile-alt mr-2"></i> Proceed Pyment</button> </p>
+                                <p> <button type="button" class="btn btn-primary "><i class="fas fa-mobile-alt mr-2"></i> Proceed Payment</button> </p>
                             </div>
                             <p class="text-muted">Note: After clicking on the button, you will be directed to a secure gateway for payment. After completing the payment process, you will be redirected back to the website to view details of your order. </p>
                         </div> <!-- End -->
